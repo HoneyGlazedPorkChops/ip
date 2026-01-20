9a47dddbde64e676167f5f71975141309f4e98b4
HoneyGlazedPorkChops <eugeneong3@gmail.com>
2026-01-20 21:38:14 +0800

    Added echo feature

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Iris {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n"
@@ -7,6 +9,28 @@ public class Iris {
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________"
         );
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Your command: ");
+            String input = scanner.nextLine().trim();
+
+            if (input.equalsIgnoreCase("Bye")) {
+                System.out.println("____________________________________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________"
+                );
+                break;
+            }
+
+            System.out.println("____________________________________________________________\n"
+                            + input + "\n"
+                            + "____________________________________________________________"
+            );
+        }
+
+        scanner.close();
     }
 }
 
