794737e957fc31037ddad9b135ed65a9bd01da07
HoneyGlazedPorkChops <eugeneong3@gmail.com>
2026-01-20 21:14:24 +0800

    Renamed bot

---
+++ src/main/java/Iris.java
@@ -0,0 +1,12 @@
+public class Iris {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n"
+                + "Hello! I'm Iris\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________"
+        );
+    }
+}
+
